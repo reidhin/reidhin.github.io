069fe30188d033d873ea7126f6d1984b1c6c352c
reidhin <hansweda@gmail.com>
2026-08-29 15:15:17 +0200

    Update patents

--- src/extract_patents.R
+++ src/extract_patents.R
@@ -2,10 +2,11 @@
 # extract patents and save in file
 url = "https://worldwide.espacenet.com/searchResults?compact=true&page=0&ST=advanced&AB=&PR=&IN=weda&rnd=1717927084587&locale=en_EP&AP=&PA=philips+OR+orikami&PD=&TI=&CPC=&Submit=Search&IC=&DB=EPODOC&PN="
 
-df <- read.csv(file.path("inst", "extdata", "results_patents.csv"))
-df <- read.csv2(file.path("inst", "extdata", "Espacenet_search_result_20240609_1148.csv"), skip=7, header=F)
-colnames(df) <- df[1,]
-df <- df[-1,]
+# df <- read.csv(file.path("inst", "extdata", "results_patents.csv"))
+# df <- read.csv2(file.path("inst", "extdata", "Espacenet_search_result_20240609_1148.csv"), skip=7, header=F)
+df <- read.csv(file.path("inst", "extdata", "Espacenet_search_result_20260829.csv"))
+# colnames(df) <- df[1,]
+# df <- df[-1,]
 
 out = c()
 
@@ -27,11 +28,11 @@ out = c(out, "  </thead>")
 out = c(out, "  <tbody>")
 
 for (i in 1:nrow(df)) {
-  out = c(out, paste0("  <tr onclick=\"window.location='https://worldwide.espacenet.com/patent/search?q=pn%3D", sub("\n", "%20", df[i, "Publication number"]), "'\">"))
+  out = c(out, paste0("  <tr onclick=\"window.location='https://worldwide.espacenet.com/patent/search?q=pn%3D", sub("\n", "%20", gsub("\\s|\\(|\\)", "", strsplit(df[i, "Publication.number"], ";")[[1]][1])), "'\">"))
   # out = c(out, paste0("  <th scope='row'>", i, "</th>"))
   out = c(out, paste0("  <td>", df[i, "Title"], "</td>"))
-  out = c(out, paste0("  <td>", df[i, "Publication number"], "</td>"))
-  out = c(out, paste0("  <td>", df[i, "Publication date"], "</td>"))
+  out = c(out, paste0("  <td>", df[i, "Publication.number"], "</td>"))
+  out = c(out, paste0("  <td>", df[i, "Publication.date"], "</td>"))
   out = c(out, "  </tr>")
 }
 
